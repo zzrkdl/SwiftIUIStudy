dfdf25d1a1eda005c4be5328c62172aacbdd6fef
김성진 <zzrkdl@naver.com>
2022-08-08 23:05:16 +0900

    Initial Commit

---
+++ Ex04_Stack/Ex04_StackApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ex04_StackApp.swift
+//  Ex04_Stack
+//
+//  Created by 김성진 on 2022/08/08.
+//
+
+import SwiftUI
+
+@main
+struct Ex04_StackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

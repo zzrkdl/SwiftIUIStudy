abf0c505f6453466da17d44c1965d26ebc79c03e
김성진 <zzrkdl@naver.com>
2022-08-13 19:43:09 +0900

    Initial Commit

---
+++ Ex20_Appstorage/Ex20_AppstorageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ex20_AppstorageApp.swift
+//  Ex20_Appstorage
+//
+//  Created by 김성진 on 2022/08/13.
+//
+
+import SwiftUI
+
+@main
+struct Ex20_AppstorageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

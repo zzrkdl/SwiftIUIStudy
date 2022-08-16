2c5d7da2a0ef0c46e661351d0fcaa3f1202821b3
김성진 <zzrkdl@naver.com>
2022-08-16 23:12:27 +0900

    Initial Commit

---
+++ Ex24_TapView/Ex24_TapViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ex24_TapViewApp.swift
+//  Ex24_TapView
+//
+//  Created by 김성진 on 2022/08/16.
+//
+
+import SwiftUI
+
+@main
+struct Ex24_TapViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

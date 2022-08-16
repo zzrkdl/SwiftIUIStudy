5a58377853febad9c705977f97ab87a34dbf5716
김성진 <zzrkdl@naver.com>
2022-08-16 23:51:26 +0900

    Initial Commit

---
+++ Ex25_Alert/Ex25_AlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ex25_AlertApp.swift
+//  Ex25_Alert
+//
+//  Created by 김성진 on 2022/08/16.
+//
+
+import SwiftUI
+
+@main
+struct Ex25_AlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
